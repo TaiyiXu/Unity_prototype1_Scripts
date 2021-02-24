7e9f9fc9830f84c0bc4a97318a4643890dacba5a
lunjielee <q531303226@gmail.com>
2021-02-24 08:20:10 -0500

    added HP text

--- GroupProject/Assets/__Scripts/PlayerController.cs
+++ GroupProject/Assets/__Scripts/PlayerController.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
+    public TextMeshProUGUI healthText;
+
     public float speed = 3.0f;
 
     public int maxHealth = 5;
     public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
-    public int currentHealth;
+    int currentHealth;
 
     bool isInvincible;
     float invincibleTimer;
 
+    public GameObject projectilePrefab;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        SetHealthText();
     }
 
     // Update is called once per frame
@@ -35,6 +43,11 @@ public class PlayerController : MonoBehaviour
             invincibleTimer -= Time.deltaTime;
             if (invincibleTimer < 0)
                 isInvincible = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Launch();
         }
     }
     private void FixedUpdate()
@@ -58,6 +71,21 @@ public class PlayerController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        SetHealthText();
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+    }
+
+    void SetHealthText()
+    {
+        healthText.text = "HP: " + health.ToString();
     }
 }
---
+++ GroupProject/Assets/__Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
