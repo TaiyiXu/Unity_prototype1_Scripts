f0f52df1a09cef0eef9ea9a22fb2be2759487ab8
lunjielee <q531303226@gmail.com>
2021-04-13 12:34:11 -0400

    now can go to Level1

--- Assets/__Scripts/LoadNewArea.cs
+++ Assets/__Scripts/LoadNewArea.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadNewArea : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string sceneName;
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Player obj = collision.gameObject.GetComponent<Player>();
+        if (obj != null)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
     }
 }
